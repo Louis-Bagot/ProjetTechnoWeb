21de2b66759de2c5e3490210eccb887cb5c762fa
Louis-Bagot <louis.bagot@insa-rouen.fr>
2018-04-23 18:10:13 +0200

    presque tout joli

--- header.php
+++ header.php
@@ -1,7 +1,11 @@
-<header>
-  <a href="PageAccueil.html"><img src="images/logoComplet.png"></a>
-  <div id="SignInUp">
-    <a href="SignIn.html"</a><button>Sign In</button></a>
-    <a href="SignUp.html"</a><button>Sign Up</button></a>
-  </div>
-</header>
+<?php
+  <header>
+    <a href="PageAccueil.html"><img src="images/logoComplet.png"></a>
+    <div id="SignInUp">
+      <a href="SignIn.html"</a><button>Sign In</button></a>
+      <a href="SignUp.html"</a><button>Sign Up</button></a>
+    </div>
+  </header>
+  <hr id="headerHR">
+  <br>
+ ?>
