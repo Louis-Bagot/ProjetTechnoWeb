69f4e1801b5f7c3b4100db44811c7f4b19018bf3
Couret Laura <lcouret@asi-testing>
2018-04-27 13:31:12 +0200

    sans les images png

--- header.php
+++ header.php
@@ -1,11 +1,11 @@
 <?php
-  <header>
-    <a href="PageAccueil.html"><img src="images/logoComplet.png"></a>
-    <div id="SignInUp">
-      <a href="SignIn.html"</a><button>Sign In</button></a>
-      <a href="SignUp.html"</a><button>Sign Up</button></a>
-    </div>
-  </header>
-  <hr id="headerHR">
+<header>
+  <a href="PageAccueil.html"><img src="images/logoComplet.png"></a>
+  <div id="SignInUp">
+    <a href="SignIn.html"</a><button>Sign In</button></a>
+    <a href="SignUp.html"</a><button>Sign Up</button></a>
+  </div>
+</header>
+<hr id="headerHR">
   <br>
  ?>
