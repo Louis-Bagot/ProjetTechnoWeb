d4d1fe81632db06b47e3513c20953f82de4e9a9e
Lefrancois Lea <llefrancois@asi-testing>
2018-04-27 16:03:15 +0200

    filtrage

---
+++ SQLite-Selection-F.php
@@ -0,0 +1,49 @@
+<?php require("login.in.php") ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    <title>Page PHP</title>
+  </head>
+  <body>
+    <?php
+      error_reporting(E_ALL);
+
+      if(isset ($_POST ["action"]) {
+        $requete = "SELECT * FROM Voyage WHERE pays= \"$_POST [\"Destination\"]\""; }
+
+        if (!empty ( $_POST["Logement"])) {
+          $requete = $requete." AND logement=\"$_POST[\"Logement\"])\"";
+        }
+
+        if (!empty ( $_POST["Pension"])) {
+          $requete = $requete." AND pension=\"$_POST[\"Pension\"])\"";
+        }
+
+        if (!empty ( $_POST["Duréemin"] & $_POST["DuréeMax"])) {
+
+        $requete = $requete." AND duree=\"$_POST[\"Durée\"])\"";
+        }
+
+        $requete = $requete.";";
+
+        try {
+            /* creation de la BD */
+            $db = new PDO("sqlite:./data/VoyageVoyage.sqlite");
+
+            /* errors -> exceptions */
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $resultF = $db->query($requete);
+            unset($db);
+            foreach($resultF as $row) {
+              echo '<p>'.$row['nomv'].' - '.$row['logement'].' - '.$row['pension'].' - '.$row['duree'].' - '.$row['urlto'].'</p>';
+            }
+        } catch(Exception $e) {
+          echo $e->getMessage();
+          echo "exception acces BD Filtrage"
+      }
+    }
+    ?>
+  </body>
+</html>
