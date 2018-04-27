1f4a06584681f712edb3b96f566c6e82850e0232
Couret Laura <lcouret@asi-testing>
2018-04-27 14:09:10 +0200

    traitement filtrage debut

---
+++ traitementFiltrage.php
@@ -0,0 +1,24 @@
+<?php
+		if(isset ($_POST ["action"]) {
+
+			if (empty ($_POST ["Destination"]) && empty ( $_POST["Logement"]) && empty ( $_POST ["texte"]) && empty ( $_POST ["Durée"])) {
+
+			//rien n'est coché comme critère : on ne fait rien
+
+			}
+
+			else if (!empty ($_POST ["Destination"]) && !empty ( $_POST["Logement"]) && !empty ( $_POST ["texte"]) && !empty ( $_POST ["Durée"])) {
+
+			//requete dans le cas où tous les champs sont remplis
+
+			}
+
+			else if (  ) {
+
+			}
+
+
+
+		}
+		include("PageAccueil.php");
+	?>
